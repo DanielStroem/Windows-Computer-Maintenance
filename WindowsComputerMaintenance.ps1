5efb034cd9b33313f735303ed39790c4408e2575
DanielStroem <92923306+DanielStroem@users.noreply.github.com>
2025-11-20 15:01:18 +0100

    Added WindowsComputerMaintenance.ps1

---
+++ WindowsComputerMaintenance.ps1
@@ -0,0 +1,21 @@
+﻿#YOU NEED ADMIN PRIVILEGES TO RUN THIS SCRIPT
+
+#Checks if computer is domain joined and updates GPOs
+$domainJoined = (Get-CimInstance Win32_ComputerSystem).PartOfDomain
+if ($domainJoined -eq $true) {
+    gpupdate /force
+}
+
+#Checks image health and repairs image if corrupt
+$health = (Repair-WindowsImage -Online -CheckHealth).ImageHealthState
+if ($health -ne "Healthy") {
+    $restart = (Repair-WindowsImage -Online -RestoreHealth -NoRestart).RestartNeeded
+}
+
+#Scans and repairs system files
+sfc /scannow
+
+#Checks if restart is needed, and restarts if confirmation is given
+if ($restart -eq $true) {
+    Restart-Computer -Confirm
+}
